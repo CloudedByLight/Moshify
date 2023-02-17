62dadafe27bae702bb3567032b9168b2cda15545
CloudedByLight <ralph.etudes@gmail.com>
2023-02-17 00:13:49 -0500

    added comments

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 toggles "collapsible--expanded" class to element */
 
 const collapsibles = document.querySelectorAll(".collapsible");
+//return NodeList of all elements with "collapsible" class
+
+//iterates through NodeList, adds event Listener to each item, toggling "collapsible--expanded" class to it on-cliok
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
